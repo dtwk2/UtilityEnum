3d7e7f510c6c96a64af0c0ccb8fd7e27eb0fb546
Declan Taylor <declan.taylor@pcs-instruments.com>
2020-04-21 09:46:01 +0100

    Added DialogResult

---
+++ UtilityEnum/DialogResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityEnum
+{
+   public enum DialogResult
+   {
+      None,
+      Ok=1,
+      Cancel
+
+   }
+}
